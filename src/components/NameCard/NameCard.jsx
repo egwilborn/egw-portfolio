fc03877813ea3cf441a2a0fe2d1ac9b8499f9045
egwilborn <egwilborn@gmail.com>
2023-08-30 12:25:04 -0500

    starts responsive design mobile

--- src/components/NameCard/NameCard.jsx
+++ src/components/NameCard/NameCard.jsx
@@ -2,20 +2,7 @@ import { Image } from "semantic-ui-react";
 import "./NameCard.css";
 export default function NameCard() {
   return (
-    <div
-      style={{
-        width: "70vw",
-        height: "50vh",
-        backgroundColor: "rgb(30 41 55)",
-        padding: "10px 10px 10px 10px",
-        margin: "5vh 10px 5vh 5px",
-        borderWidth: "0px",
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <div className="name-card">
       <div
         style={{
           display: "flex",
@@ -25,23 +12,14 @@ export default function NameCard() {
         }}
       >
         <Image
+          className="name-design"
           src="https://i.imgur.com/2J1Pham.png"
           floated="left"
-          style={{ width: "30vw", height: "30vh", maxWidth: "300px" }}
+          style={{ maxWidth: "300px" }}
         />
         <div style={{ color: "rgb(154 144 152)", width: "30vw" }}>
-          <p
-            className="FirstName"
-            style={{ textAlign: "left", fontSize: "4vw", marginLeft: "30px" }}
-          >
-            Emily
-          </p>
-          <p
-            className="LastName"
-            style={{ textAlign: "right", fontSize: "4vw" }}
-          >
-            Wilborn
-          </p>
+          <p className="FirstName">Emily</p>
+          <p className="LastName">Wilborn</p>
         </div>
       </div>
     </div>
--- src/components/Nav Bar/NavBar.jsx
+++ src/components/Nav Bar/NavBar.jsx
@@ -16,51 +16,30 @@ export default function NavBar() {
         color: "rgb(208 208 210)",
       }}
     >
-      <Menu.Item
-        className="NavBar"
-        style={{ color: "rgb(208 208 210)", fontSize: "3vw" }}
-        name="EGW"
-      >
+      <Menu.Item className="NavBar-name" name="EGW">
         <HashLink smooth to={"/#top"}>
           EGW
         </HashLink>
       </Menu.Item>
-      <Menu.Item
-        className="NavBar"
-        style={{ color: "rgb(208 208 210)" }}
-        position="right"
-        name="Skills"
-      >
+      <Menu.Item className="NavBar" position="right" name="Skills">
         <HashLink smooth to={"/#skills-section"}>
           Skills
         </HashLink>
       </Menu.Item>
       •
-      <Menu.Item
-        className="NavBar"
-        style={{ color: "rgb(208 208 210)" }}
-        name="Projects"
-      >
+      <Menu.Item className="NavBar" name="Projects">
         <HashLink smooth to={"/#project-section"}>
           Projects
         </HashLink>
       </Menu.Item>
       •
-      <Menu.Item
-        className="NavBar"
-        style={{ color: "rgb(208 208 210)" }}
-        name="About Me"
-      >
+      <Menu.Item className="NavBar" name="About Me">
         <HashLink smooth to={"/#about-section"}>
           About Me
         </HashLink>
       </Menu.Item>
       •
-      <Menu.Item
-        className="NavBar"
-        style={{ color: "rgb(208 208 210)" }}
-        name="Contact"
-      >
+      <Menu.Item className="NavBar" name="Contact">
         <HashLink smooth to={"/#contact-section"}>
           Contact
         </HashLink>

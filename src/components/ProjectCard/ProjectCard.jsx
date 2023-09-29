5fc69e63d78a67808a38373c0a8cb5ee34b0729a
egwilborn <egwilborn@gmail.com>
2023-09-28 21:39:09 -0500

    adjusts responsiveness to new info

--- src/components/ProjectCard/ProjectCard.jsx
+++ src/components/ProjectCard/ProjectCard.jsx
@@ -29,16 +29,14 @@ export default function ProjectCard() {
             className="project-image"
             src="https://i.imgur.com/MTm1RLg.png"
           ></Image>
-
+          <div className="project-technologies">
+            JavaScript • React • Node.js • Express • MongoDB
+          </div>
           <div className="project-text">
             <p>
               Travelog is an app to make your travel wishlist and help you plan
               your trip.
             </p>
-          </div>
-
-          <div className="project-technologies">
-            JavaScript • React • Node.js • Express • MongoDB • Semantic • AWS
           </div>
         </div>
 
@@ -46,24 +44,31 @@ export default function ProjectCard() {
           className="project-card"
           style={{ backgroundColor: "rgb(66 34 57)" }}
         >
+          <span className="project-title">Novel Nest</span>
+          <div className="project-links">
+            <a className="project-link" href="https://novelnest.fly.dev/">
+              Live Site
+            </a>
+            •
+            <a
+              className="project-link"
+              href="https://github.com/egwilborn/project-4-novel-nest"
+            >
+              GitHub
+            </a>
+          </div>
           <Image
             className="project-image"
             src="https://i.imgur.com/TYFQ0Ak.png"
           ></Image>
+          <div className="project-technologies">
+            Python • Django • PostgreSQL • Materialize • DTL
+          </div>
           <div className="project-text">
-            <span style={{ color: "rgb(208 208 210)" }}>Novel Nest</span>
-            <div className="project-links">
-              <a className="project-link" href="https://novelnest.fly.dev/">
-                Live Site
-              </a>
-              •
-              <a
-                className="project-link"
-                href="https://github.com/egwilborn/project-4-novel-nest"
-              >
-                GitHub
-              </a>
-            </div>
+            <p>
+              Users can sign up to browse a large selection of e-books,
+              organized by genre.
+            </p>
           </div>
         </div>
 
@@ -72,6 +77,23 @@ export default function ProjectCard() {
           style={{ backgroundColor: "rgb(32 41 54)" }}
         >
           <span className="project-title">MyPlanner</span>
+          <div className="project-links">
+            <Popup
+              content="Not Yet Deployed"
+              trigger={
+                <a className="Link" href="#">
+                  Live Site
+                </a>
+              }
+            />
+            •
+            <a
+              className="project-link"
+              href="https://github.com/egwilborn/planner"
+            >
+              GitHub
+            </a>
+          </div>
           <Image
             className="project-image"
             src="https://i.imgur.com/4AabJYh.png"
@@ -82,51 +104,37 @@ export default function ProjectCard() {
           <div className="project-text">
             <p>MyPlanner is a productivity tool for making To Do Lists.</p>
           </div>
-          <div className="project-text">
-            <div className="project-links">
-              <Popup
-                content="Not Yet Deployed"
-                trigger={
-                  <a className="Link" href="#">
-                    Live Site
-                  </a>
-                }
-              />
-              •
-              <a
-                className="project-link"
-                href="https://github.com/egwilborn/planner"
-              >
-                GitHub
-              </a>
-            </div>
-          </div>
         </div>
+
         <div
           className="project-card"
           style={{ backgroundColor: "rgb(66 34 57)" }}
         >
+          <span className="project-title"> RadioDB </span>
+          <div className="project-links">
+            <a
+              className="project-link"
+              href="https://long-pear-seahorse-suit.cyclic.app/podcasts"
+            >
+              Live Site
+            </a>
+            •
+            <a
+              className="project-link"
+              href="https://github.com/egwilborn/unit-2-project"
+            >
+              GitHub
+            </a>
+          </div>
           <Image
             className="project-image"
             src="https://i.imgur.com/wL2PaBe.png"
           ></Image>
+          <div className="project-technologies">
+            Express.js • Node.js • MongoDB • JavaScript • EJS
+          </div>
           <div className="project-text">
-            <span style={{ color: "rgb(208 208 210)" }}> RadioDB </span>
-            <div className="project-links">
-              <a
-                className="project-link"
-                href="https://long-pear-seahorse-suit.cyclic.app/podcasts"
-              >
-                Live Site
-              </a>
-              •
-              <a
-                className="project-link"
-                href="https://github.com/egwilborn/unit-2-project"
-              >
-                GitHub
-              </a>
-            </div>
+            <p>Browse RadioDb to see what podcasts people are listening to.</p>
           </div>
         </div>
 
@@ -134,28 +142,31 @@ export default function ProjectCard() {
           className="project-card"
           style={{ backgroundColor: "rgb(32 41 54)" }}
         >
+          <span className="project-title">Wilted Word</span>
+          <div className="project-links">
+            <a
+              className="project-link"
+              href="https://egwilborn.github.io/browser-game-project/"
+            >
+              Live Site
+            </a>
+            •
+            <a
+              className="project-link"
+              href="https://github.com/egwilborn/browser-game-project"
+            >
+              GitHub
+            </a>
+          </div>
           <Image
             className="project-image"
-            src="https://i.imgur.com/sy5aCxA.png"
+            src="https://i.imgur.com/jHlnjr8.png"
           ></Image>
+          <div className="project-technologies">
+            HTML • CSS • JavaScript • MVC
+          </div>
           <div className="project-text">
-            <span style={{ color: "rgb(208 208 210)" }}>Wilted Word</span>
-            <p></p>
-            <div className="project-links">
-              <a
-                className="project-link"
-                href="https://egwilborn.github.io/browser-game-project/"
-              >
-                Live Site
-              </a>
-              •
-              <a
-                className="project-link"
-                href="https://github.com/egwilborn/browser-game-project"
-              >
-                GitHub
-              </a>
-            </div>
+            <p>Wilted Word is a flowery reimagining of the game hangman.</p>
           </div>
         </div>
       </div>
